74587b8cf434c0e0733c723c8279ec3a75510346
Christopher kola <Christopherkola@gmail.com>
2026-05-22 01:25:55 +0200

    [FEAT]: add Operator entity with id, name, email, and assets relationship

--- src/main/java/com/christopher/fleet_management/operator/Operator.java
+++ src/main/java/com/christopher/fleet_management/operator/Operator.java
@@ -1,5 +1,39 @@
 package com.christopher.fleet_management.operator;
 
+import java.util.List;
+
+import com.christopher.fleet_management.asset.Asset;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Operator {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "Name is required")
+	private String name;
+
+	@NotBlank(message = "Email is required")
+	@Email(message = "Email must be valid")
+	private String email;
+
+	@OneToMany(mappedBy = "operator")
+	private List<Asset> assets;
 }
